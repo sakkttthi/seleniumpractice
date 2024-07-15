074cced6784aead7cd6ec909e8aee9b485f3503c
sakthivel.chandru <sakthivel.chandru@SWPC164.gswvss.com>
2024-07-15 10:55:12 +0530

    First commit

---
+++ src/test/java/day1/practice/Basics.java
@@ -0,0 +1,45 @@
+package day1.practice;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Basics {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		driver.manage().window().maximize();
+		
+		System.out.println(driver.getCurrentUrl());
+	
+		System.out.println(driver.getTitle());
+		
+		System.out.println(driver.getPageSource());
+		
+		driver.get("https://www.bing.com/");
+		System.out.println(driver.getCurrentUrl());
+		
+		driver.navigate().back();
+		System.out.println(driver.getCurrentUrl());
+		
+		System.out.println(driver.getWindowHandle());
+		
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		List<WebElement> elements = driver.findElements(By.xpath("//div[@class='oxd-layout-context']/div/div"));
+		System.out.println(elements);
+		
+		
+		driver.quit();
+		
+	}
+
+}
